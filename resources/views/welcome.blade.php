8675d73756811f6c2e37cb0e6e72db3741b98b2f
MyUsername <samuele.maddaluno1@gmail.com>
2025-12-14 20:21:13 +0100

    Inizializzazione progetto TALL:
    - Creazione file di progetto
    - Configurazione database MySQL e collegamento con Laravel
    - Gestione migrazioni e creazione dei seed di prova
    - Installazione dipendenze necessarie
    - Modifica della welcome page con Tailwind CSS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<x-guest-layout>
+    <div class="flex flex-col bg-green-800 w-full h-screen">
+        <nav class="flex pt-5 justify-between container mx-auto text-green-200">
+            <a class="text-2xl font-bold" href="/">
+                <x-application-logo class="w-16 h-16 fill-current">
+                </x-application-logo>
+            </a>
+            <div class="flex justify-end">
+                @auth
+
+                    <a href="{{ route('dashboard')}}">
+                    Dashboard</a>
+
+                @else
+
+                    <a href="{{ route('login')}}">
+                    Login</a>
+
+                @endauth
+            </div>
+        </nav>
+    </div>
+    <div class="flex flex-col bg-pink-800 w-full h-screen">
+
+    </div>
+</x-guest-layout>
