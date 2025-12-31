9293a18091d946430b70df6af6d762a08d3cf4bd
MyUsername <samuele.maddaluno1@gmail.com>
2025-12-31 11:38:22 +0100

    modifica layout welcome in profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,44 @@
                 @endauth
             </div>
         </nav>
+
+        <div class="flex container mx-auto items-center h-full">
+            <div class="flex flex-col w-1/3 items-start">
+
+                <h1 class="text-white font-bolds text-5xl leading-tight mb-4">
+                   Pagina di iscrizione generica
+                </h1>
+
+                <p class="tex-green-200 text-xl mb-10">
+                    Stiamo verificando le stack <span class="font-bold underline">TALL</span>. ti interessa iscriverti?
+                </p>
+
+                <x-primary-button class="py-3 px-8 bg-red-700 hover:bg-red-600">
+                    iscriviti
+                </x-primary-button>
+            </div>
+        </div>
     </div>
     <div class="flex flex-col bg-pink-800 w-full h-screen">
-
+        <p class="text-white text-5xl
+        font-extrabold text-center">
+            Let's do it!
+        </p>
+        <form class="flex flex-col items-center p-20">
+            <x-text-input
+            class="px-5 py-3 w-80 
+            border-blue-400"
+            type="email"
+            name="email"
+            placeholder="Email"
+            ></x-text-input>
+            <span class="text-gray-100 text-xs">
+                Ti spediremo un email di conferma.
+            </span>
+            <x-primary-button class="px-5 py-3 mt-5
+            w-80 bg-blue-500 justify-center">
+                entra
+            </x-primary-button>
+        </form>
     </div>
 </x-guest-layout>
