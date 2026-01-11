c18d809c602fb7ae65a42d4f867f7d43022c9056
MyUsername <samuele.maddaluno1@gmail.com>
2026-01-11 12:11:18 +0100

    ottimizzazione UX su modale e stati di caricamento. Implementato x-cloak e relativo CSS per prevenire il FOUC  al caricamento della pagina. Aggiunto indicatore di caricamento  con target specifico sul metodo subscribe per fornire feedback visivo durante l'invio del form.

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -4,7 +4,9 @@
         bg-opacity-60 items-center w-full h-full"
         x-show="{{$trigger}}"
         @click.self="{{$trigger}} = false"
-        x-on:keydown.escape.window="{{$trigger}} = false" >
+        x-on:keydown.escape.window="{{$trigger}} = false" 
+        x-cloak
+        >
 
             <div {{ $attributes ->merge([ 'class'=>'m-auto bg-gray-200 shadow-2xl rounded-xl p-8' ]) }}>
 
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -61,7 +61,7 @@
                     name="email"
                     autocomplete="email"
                     placeholder="Email"
-                    wire:model="email"
+                    wire:model.defer="email"
                     ></x-text-input>
 
                     <span class="text-gray-100 text-xs">
@@ -75,7 +75,13 @@
                     type="submit" 
                     class="px-5 py-3 mt-5
                     w-80 bg-blue-500 justify-center">
-                        conferma
+
+                        <span class="animate-spin" wire:loading wire:target="subscribe">
+                            &#9696;
+                        </span>
+                        <span wire:loading.remove wire:target="subscribe">
+                            conferma
+                        </span>
                     </x-primary-button>
               
                 </form>
